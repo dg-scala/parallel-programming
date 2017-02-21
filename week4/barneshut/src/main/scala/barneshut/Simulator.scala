49710a837911c79f81fb6af9724272b5f79fdea1
Dragan Glumac <dragan.glumac@gmail.com>
2017-02-21 21:43:43 +0000

    Passed all grader test.

--- week4/barneshut/src/main/scala/barneshut/Simulator.scala
+++ week4/barneshut/src/main/scala/barneshut/Simulator.scala
@@ -47,7 +47,7 @@ class Simulator(val taskSupport: TaskSupport, val timeStats: TimeStatistics) {
   def updateBodies(bodies: Seq[Body], quad: Quad): Seq[Body] = timeStats.timed("update") {
     val parBodies = bodies.par
     parBodies.tasksupport = taskSupport
-    ???
+    parBodies.aggregate(Seq.empty[Body])((bds, b) => bds :+ b.updated(quad), (bds1, bds2) => bds1 ++ bds2)
   }
 
   def eliminateOutliers(bodies: Seq[Body], sectorMatrix: SectorMatrix, quad: Quad): Seq[Body] = timeStats.timed("eliminate") {
